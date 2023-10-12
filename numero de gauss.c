5921016fe1b6c7b4135245bc9f9932b510231f86
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-12 16:34:59 -0500

    estructura repetitiva FOR

--- numero de gauss.c
+++ numero de gauss.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(){
-    int n,j,s=0
-    printf("Colocar el numero entero que se desea saber el numero de Gauss: ");
-    scanf("%d",&n);
 
-}
+int main() {
+    int n, j, s = 0;
+
+    printf("Colocar el numero entero que se desea saber el numero de Gauss: ");
+    scanf("%d", &n);
+
+    for (j = 2 * n; j >= 1; j--) {
+        s += j;
+    }
